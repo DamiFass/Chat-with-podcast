01eec21edd1a433bf67b4dfe863ed0c76c6bd644
Damiano Fassina <d.fassina18@imperial.ac.uk>
2023-12-15 17:21:25 +0000

    completed notebooks

--- openai_interface.py
+++ openai_interface.py
@@ -9,7 +9,7 @@ _ = load_dotenv('./.env', override=True) # read local .env file
 
 class GPT_Turbo:
 
-    def __init__(self, model: str="gpt-3.5-turbo-0613", api_key: str=os.environ['OPENAI_API_KEY']):
+    def __init__(self, model: str="gpt-3.5-turbo-0613", api_key: str='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'):
         self.model = model
         self.client = OpenAI(api_key=api_key)
 
